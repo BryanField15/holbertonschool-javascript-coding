c959b52757643598128117a3629dd35f90297424
BryanField15 <4bryanfield@gmail.com>
2023-10-10 11:09:17 +1100

    Updated code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
